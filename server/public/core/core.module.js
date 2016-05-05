635a184de2ba0ca2901375b08f9e02a589ac0c9f
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-05-04 19:37:35 -0700

    Install Angular Socket IO

--- server/public/core/core.module.js
+++ server/public/core/core.module.js
@@ -2,5 +2,5 @@
   'use strict'
 
   angular
-    .module('jubilant-umbrella.core', ['ngMaterial', 'ui.router', 'uiGmapgoogle-maps'])
+    .module('jubilant-umbrella.core', ['btford.socket-io', 'ngMaterial', 'ui.router', 'uiGmapgoogle-maps'])
 })()
