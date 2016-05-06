b4d1c6abb8b3e206b16b152e1a37d4a170b5f975
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-05-06 09:56:53 -0700

    Add Test Data

--- server/public/services/emergencies.factory.js
+++ server/public/services/emergencies.factory.js
@@ -8,6 +8,7 @@
   function Emergencies () {
     return [
       {
+        'uniqueID': '10C',
         'timestamp': 'Apr 30, 2016 8:50:31 PM',
         'location': 'Clarksville',
         'name': 'Willie Nelson',
@@ -15,9 +16,11 @@
         'coords': {
           latitude: 30.266030,
           longitude: -97.750000
-        }
+        },
+        'active': true
       },
       {
+        'uniqueID': '32T',
         'timestamp': 'Apr 30, 2016 10:17:40 PM',
         'location': 'South Congress',
         'name': 'Sandra Bullock',
@@ -25,7 +28,32 @@
         'coords': {
           latitude: 30.266900,
           longitude: -97.740550
-        }
+        },
+        'active': true
+      },
+      {
+        'uniqueID': '58Z',
+        'timestamp': 'Apr 30, 2016 11:00:31 PM',
+        'location': 'Hyde Park',
+        'name': 'Matthew Mcconaughey',
+        'phone': '444-444-4444',
+        'coords': {
+          latitude: 30.276700,
+          longitude: -97.722300
+        },
+        'active': true
+      },
+      {
+        'uniqueID': '09F',
+        'timestamp': 'Apr 30, 2016 10:17:40 PM',
+        'location': 'Westlake',
+        'name': 'Michael Dell',
+        'phone': '555-555-5555',
+        'coords': {
+          latitude: 30.256500,
+          longitude: -97.767400
+        },
+        'active': true
       }
     ]
   }
