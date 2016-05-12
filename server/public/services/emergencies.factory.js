b0dfc544cae3c9d645d1cd62af5b0a8575553742
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-05-12 16:38:07 -0700

    Update Mock Data Format

--- server/public/services/emergencies.factory.js
+++ server/public/services/emergencies.factory.js
@@ -8,47 +8,78 @@
   function Emergencies () {
     var emergencies = [
       {
-        uniqueID: '10C',
-        started: '2016-05-08T02:32:10.000Z',
-        name: 'Kobe Bryant',
-        phone: '222-222-2222',
-        location: {
-          latitude: 34.018344,
-          longitude: -118.491285
-        },
-        active: false
-      },
-      {
-        uniqueID: '32T',
-        started: '2016-05-08T02:33:10.000Z',
-        name: 'Matt Damon',
-        phone: '333-333-3333',
-        location: {
-          latitude: 34.017452,
-          longitude: -118.490183
-        },
-        active: false
-      },
-      {
-        uniqueID: '58Z',
+        id: 'HT86',
+        by: 'Jennifer Lawrence',
+        phone: '111-111-1111',
         started: '2016-05-08T02:34:10.000Z',
-        name: 'Jennifer Lawrence',
-        phone: '444-444-4444',
-        location: {
-          latitude: 34.018439,
-          longitude: -118.488184
-        },
+        ended: null,
+        locations: [
+          {
+            timestamp: '2016-05-08T02:35:10.000Z',
+            latitude: 34.019800,
+            longitude: -118.491000
+          },
+          {
+            timestamp: '2016-05-08T02:36:10.000Z',
+            latitude: 34.019420,
+            longitude: -118.491130
+          },
+          {
+            timestamp: '2016-05-08T02:37:10.000Z',
+            latitude: 34.019440,
+            longitude: -118.49000
+          }
+        ],
+        active: true
+      },
+      {
+        id: 'BX93',
+        by: 'Joaquin Phoenix',
+        phone: '222-222-2222',
+        started: '2016-05-08T02:24:10.000Z',
+        ended: null,
+        locations: [
+          {
+            timestamp: '2016-05-08T02:25:10.000Z',
+            latitude: 34.019341,
+            longitude: -118.491382
+          },
+          {
+            timestamp: '2016-05-08T02:27:10.000Z',
+            latitude: 34.019141,
+            longitude: -118.491182
+          },
+          {
+            timestamp: '2016-05-08T02:37:10.000Z',
+            latitude: 34.019500,
+            longitude: -118.49500
+          }
+        ],
         active: false
       },
       {
-        uniqueID: '09F',
-        started: '2016-05-08T02:35:10.000Z',
-        name: 'Elon Musk',
-        phone: '555-555-5555',
-        location: {
-          latitude: 34.019441,
-          longitude: -118.491182
-        },
+        id: 'UJ39',
+        by: 'Elon Musk',
+        phone: '333-333-3333',
+        started: '2016-05-09T02:28:10.000Z',
+        ended: null,
+        locations: [
+          {
+            timestamp: '2016-05-09T02:29:10.000Z',
+            latitude: 34.017341,
+            longitude: -118.491382
+          },
+          {
+            timestamp: '2016-05-09T02:30:10.000Z',
+            latitude: 34.017541,
+            longitude: -118.491882
+          },
+          {
+            timestamp: '2016-05-09T02:31:10.000Z',
+            latitude: 34.017041,
+            longitude: -118.489382
+          }
+        ],
         active: false
       }
     ]
