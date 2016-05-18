71720fd6686b2e499b21e7c6e068c3ac6ba50ad8
Mark Castle <mark@castle.sx>
2016-05-18 12:58:58 -0700

    Write Authentication Factory

---
+++ server/public/services/authentication.factory.js
@@ -0,0 +1,33 @@
+(function () {
+  'use strict'
+
+  angular
+    .module('jubilant-umbrella')
+    .factory('Authentication', Authentication)
+
+  function Authentication ($http, $localStorage, $state) {
+    return {
+      current: function () {
+        return {
+          school: $localStorage.school,
+          token: $localStorage.token
+        }
+      },
+
+      signIn: function (school, password) {
+        $http.post('/schools/signin', {school: school, password: password})
+          .success(function (data) {
+            $localStorage.school = school
+            $localStorage.token = data.token
+            $http.defaults.headers.common['token'] = data.token
+            $state.go('dashboard')
+          })
+      },
+
+      signOut: function () {
+        $localStorage.$reset()
+        $state.go('landing')
+      }
+    }
+  }
+})()
