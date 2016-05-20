c7a176110c80f1e6d8c8e6121f7b11121e2ea78f
Mark Castle <mark@castle.sx>
2016-05-19 17:02:52 -0700

    Write Fixtures

---
+++ server/fixtures.js
@@ -0,0 +1,261 @@
+var emergencies = {
+  MakerSquare: [
+    {
+      id: 'HT86F',
+      by: 'Jennifer Lawrence',
+      phone: '111-111-1111',
+      started: '5/18/16 4:00 PM',
+      ended: '5/18/16 4:10 PM',
+      locations: [
+        {
+          timestamp: '5/18/16 4:00 PM',
+          latitude: '34.013090',
+          longitude: '-118.495138'
+        },
+        {
+          timestamp: '5/18/16 4:03 PM',
+          latitude: '34.013168',
+          longitude: '-118.495042'
+        },
+        {
+          timestamp: '5/18/16 4:04 PM',
+          latitude: '34.016216',
+          longitude: '-118.498619'
+        },
+        {
+          timestamp: '5/18/16 4:08 PM',
+          latitude: '34.019453',
+          longitude: '-118.494635'
+        },
+        {
+          timestamp: '5/18/16 4:10 PM',
+          latitude: '34.019394',
+          longitude: '-118.494564'
+        }
+      ],
+      active: false
+    },
+    {
+      id: 'BX93I',
+      by: 'Joaquin Phoenix',
+      phone: '222-222-2222',
+      started: '5/17/16 10:02 PM',
+      ended: '5/17/16 10:08 PM',
+      locations: [
+        {
+          timestamp: '5/18/16 10:02 PM',
+          latitude: '34.022784',
+          longitude: '-118.496577'
+        },
+        {
+          timestamp: '5/18/16 10:05 PM',
+          latitude: '34.023030',
+          longitude: '-118.495571'
+        },
+        {
+          timestamp: '5/18/16 10:08 PM',
+          latitude: '34.022188',
+          longitude: '-118.494572'
+        }
+      ],
+      active: false
+    },
+    {
+      id: 'UJ39G',
+      by: 'Elon Musk',
+      phone: '333-333-3333',
+      started: '5/17/16 9:14 PM',
+      ended: '5/17/16 9:20 PM',
+      locations: [
+        {
+          timestamp: '5/18/16 9:14 PM',
+          latitude: '34.024546',
+          longitude: '-118.498195'
+        },
+        {
+          timestamp: '5/18/16 9:15 PM',
+          latitude: '34.024128',
+          longitude: '-118.498688'
+        },
+        {
+          timestamp: '5/18/16 9:17 PM',
+          latitude: '34.023555',
+          longitude: '-118.498022'
+        },
+        {
+          timestamp: '5/18/16 9:18 PM',
+          latitude: '34.023203',
+          longitude: '-118.498461'
+        },
+        {
+          timestamp: '5/18/16 9:19 PM',
+          latitude: '34.023080',
+          longitude: '-118.498300'
+        },
+        {
+          timestamp: '5/18/16 9:20 PM',
+          latitude: '34.023150',
+          longitude: '-118.498195'
+        }
+      ],
+      active: false
+    },
+    {
+      id: 'YX93T',
+      by: 'Emma Watson',
+      phone: '444-444-4444',
+      started: '5/16/16 11:20 PM',
+      ended: '5/16/16 11:30 PM',
+      locations: [
+        {
+          timestamp: '5/16/16 11:20 PM',
+          latitude: '34.020063',
+          longitude: '-118.502188'
+        },
+        {
+          timestamp: '5/16/16 11:23 PM',
+          latitude: '34.019352',
+          longitude: '-118.501373'
+        },
+        {
+          timestamp: '5/16/16 11:26 PM',
+          latitude: '34.018642',
+          longitude: '-118.502309'
+        },
+        {
+          timestamp: '5/16/16 11:27 PM',
+          latitude: '34.018838',
+          longitude: '-118.502554'
+        }
+      ],
+      active: false
+    },
+    {
+      id: 'SX42U',
+      by: 'Woody Allen',
+      phone: '555-555-5555',
+      started: '5/16/16 2:30 AM',
+      ended: '5/16/16 2:45 AM',
+      locations: [
+        {
+          timestamp: '5/16/16 2:30 AM',
+          latitude: '34.019015',
+          longitude: '-118.493125'
+        },
+        {
+          timestamp: '5/16/16 2:35 AM',
+          latitude: '34.019180',
+          longitude: '-118.492869'
+        },
+        {
+          timestamp: '5/16/16 2:40 AM',
+          latitude: '34.017796',
+          longitude: '-118.491247'
+        },
+        {
+          timestamp: '5/16/16 2:45 AM',
+          latitude: '34.017707',
+          longitude: '-118.491381'
+        }
+      ],
+      active: false
+    },
+    {
+      id: 'DZ02F',
+      by: 'Bill Burr',
+      phone: '666-666-6666',
+      started: '5/15/16 8:56 PM',
+      ended: '5/15/16 9:04 PM',
+      locations: [
+        {
+          timestamp: '5/16/16 8:56 PM',
+          latitude: '34.022866',
+          longitude: '-118.497069'
+        },
+        {
+          timestamp: '5/16/16 8:57 PM',
+          latitude: '34.022062',
+          longitude: '-118.498104'
+        },
+        {
+          timestamp: '5/16/16 9:00 AM',
+          latitude: '34.024715',
+          longitude: '-118.501225'
+        },
+        {
+          timestamp: '5/16/16 9:04 PM',
+          latitude: '34.023190',
+          longitude: '-118.503098'
+        }
+      ],
+      active: false
+    },
+    {
+      id: 'DZ02F',
+      by: 'Kevin Hart',
+      phone: '777-777-7777',
+      started: '5/14/16 1:01 AM',
+      ended: '5/14/16 1:08 AM',
+      locations: [
+        {
+          timestamp: '5/14/16 1:01 AM',
+          latitude: '34.023199',
+          longitude: '-118.496183'
+        },
+        {
+          timestamp: '5/14/16 1:04 AM',
+          latitude: '34.023555',
+          longitude: '-118.496167'
+        },
+        {
+          timestamp: '5/14/16 1:06 AM',
+          latitude: '34.023953',
+          longitude: '-118.495669'
+        },
+        {
+          timestamp: '5/14/16 1:09 AM',
+          latitude: '34.022857',
+          longitude: '-118.494380'
+        },
+        {
+          timestamp: '5/14/16 1:09 AM',
+          latitude: '34.023037',
+          longitude: '-118.494028'
+        }
+      ],
+      active: false
+    }
+  ]
+}
+
+var schools = [
+  {
+    name: 'MakerSquare',
+    location: {
+      latitude: '34.0192691',
+      longitude: '-118.49434429999997'
+    },
+    zoom: 16
+  },
+  {
+    name: 'UCLA',
+    location: {
+      latitude: '50',
+      longitude: '50'
+    },
+    zoom: 15
+  },
+  {
+    name: 'USC',
+    location: {
+      latitude: '75',
+      longitude: '75'
+    },
+    zoom: 20
+  }
+]
+
+module.exports = {
+  emergencies: emergencies,
+  schools: schools
+}
